4c885a9fa752d36cdbe902385b8b38f6c2e1c0a4
YazDev0 <yazed.marshad@hotmail.com>
2025-09-13 00:55:22 +0300

    .

--- Assets/Scripts/TrapsHit.cs
+++ Assets/Scripts/TrapsHit.cs
@@ -7,7 +7,7 @@ public class TrapsHit : MonoBehaviour
 {
      void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) 
+        if (collision.CompareTag("hit")) 
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
         }
