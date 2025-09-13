8b445c1fab9044c618177d6b19344bc357c34a40
YazDev0 <yazed.marshad@hotmail.com>
2025-09-14 02:44:07 +0300

    .

--- Assets/Scripts/MainMenuUI/SettingsScripts.cs
+++ Assets/Scripts/MainMenuUI/SettingsScripts.cs
@@ -15,9 +15,15 @@ public class SettingsScripts : MonoBehaviour
 
     const string VOL_KEY = "vol";
 
+    // ŞÇÆãÉ ÈÌãíÚ ãÕÇÏÑ ÇáÕæÊ İí ÇááÚÈÉ
+    private List<AudioSource> allAudioSources = new List<AudioSource>();
+
     void Awake()
     {
         if (masterVolume) masterVolume.onValueChanged.AddListener(OnVolumeChanged);
+
+        // ÇáÈÍË Úä ÌãíÚ ãÕÇÏÑ ÇáÕæÊ İí ÇáãÔåÏ
+        FindAllAudioSources();
     }
 
     void Start()
@@ -36,14 +42,46 @@ public class SettingsScripts : MonoBehaviour
 
     void OnVolumeChanged(float v)
     {
-        // Íæøá 0..1 Åáì dB (ãä -80dB Åáì 0dB)
+        // ÇáÊÍßã İí Audio Mixer
         float dB = (v <= 0.0001f) ? -80f : Mathf.Log10(v) * 20f;
         if (mixer) mixer.SetFloat(exposedParam, dB);
+
+        // ÇáÊÍßã ÇáãÈÇÔÑ İí ÌãíÚ ãÕÇÏÑ ÇáÕæÊ (ßÎØæÉ ÇÍÊíÇØíÉ)
+        SetAllAudioSourcesVolume(v);
+
         PlayerPrefs.SetFloat(VOL_KEY, v);
         PlayerPrefs.Save();
     }
 
+    // ÇáÈÍË Úä ÌãíÚ ãÕÇÏÑ ÇáÕæÊ İí ÇáãÔåÏ
+    void FindAllAudioSources()
+    {
+        AudioSource[] sources = FindObjectsOfType<AudioSource>();
+        allAudioSources.Clear();
+        allAudioSources.AddRange(sources);
+    }
 
+    // ÊÚííä volume áÌãíÚ ãÕÇÏÑ ÇáÕæÊ
+    void SetAllAudioSourcesVolume(float volume)
+    {
+        foreach (AudioSource source in allAudioSources)
+        {
+            if (source != null)
+            {
+                source.volume = volume;
+            }
+        }
+    }
 
-    public void Close() { if (panel) panel.SetActive(false); }
+    // ÊÍÏíË ŞÇÆãÉ ãÕÇÏÑ ÇáÕæÊ ÚäÏ İÊÍ ÇáÅÚÏÇÏÇÊ
+    public void OpenSettings()
+    {
+        if (panel) panel.SetActive(true);
+        FindAllAudioSources(); // ÊÍÏíË ÇáŞÇÆãÉ ÚäÏ İÊÍ ÇáÅÚÏÇÏÇÊ
+    }
+
+    public void Close()
+    {
+        if (panel) panel.SetActive(false);
+    }
 }
